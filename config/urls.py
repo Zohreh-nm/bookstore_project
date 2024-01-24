bb9c98d377041570b1a54acb6fa83ac62dda3ec6
Zohreh <zohreh.nm@gmail.com>
2024-01-24 14:57:44 -0800

    add book cover

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
@@ -24,5 +26,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('books/', include('books.urls')),
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
