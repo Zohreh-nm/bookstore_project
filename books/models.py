fcc343de557b40d95e370b64b803ce77c2b5fa2e
Zohreh <zohreh.nm@gmail.com>
2024-01-26 14:12:13 -0800

    add user who created the book object

--- books/models.py
+++ books/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
+
 class Book(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     description = models.TextField()
